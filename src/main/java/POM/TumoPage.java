48fb1fab72abf331e87b3a204644f8f408ae2e8c
Arman Margaryan <margaryan.97.38862@gmail.com>
2022-06-12 16:13:49 +0400

    test

---
+++ src/main/java/POM/TumoPage.java
@@ -0,0 +1,14 @@
+package POM;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class Tumo extends BasePage{
+    @FindBy
+
+
+    public Tumo(WebDriver driver) {
+        super(driver);
+    }
+}
